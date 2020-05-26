e23725b4b1418baac802a210908507600332cd0e
xufengFlynn <zhangxufeng26@gmail.com>
2020-05-25 17:35:36 -0700

    Initialize

---
+++ src/main/java/flynn/springframework/jokes/jokesup/JokesupApplication.java
@@ -0,0 +1,13 @@
+package flynn.springframework.jokes.jokesup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokesupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/flynn/springframework/jokes/jokesup/JokesupApplicationTests.java
@@ -0,0 +1,13 @@
+package flynn.springframework.jokes.jokesup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
